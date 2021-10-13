083ec438ebf4683931f94a85f21bbc7a30503dd9
martin.montemayor <martin.montemayor@CLQL001427>
2021-10-13 00:45:50 -0500

    Add project files.

---
+++ Lecture09/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Lecture09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\martin.montemayor\\source");
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://www.facebook.com/";
+
+            IWebElement email = driver.FindElement(By.CssSelector("[name = 'email']"));
+            IWebElement password = driver.FindElement(By.Id("pass"));
+            IWebElement login = driver.FindElement(By.Name("login"));
+
+            email.SendKeys("This is a test");
+            password.SendKeys("1234Test");
+            login.Click();
+
+            Console.WriteLine("Test Passed");
+        }
+    }
+}
---
+++ Lecture10/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.ObjectModel;
+
+
+namespace WebDriver01
+{
+    class Lecture10
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:\\Users\\martin.montemayor\\source");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Url = "https://www.google.com/";
+
+            IWebElement searchBox = driver.FindElement(By.CssSelector("[name = 'q']"));
+            searchBox.SendKeys("This is a test");
+            searchBox.SendKeys(Keys.Enter);
+
+            ReadOnlyCollection<IWebElement> allH3Elements = driver.FindElements(By.TagName("h3"));
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(allH3Elements[i].Text);
+
+            }
+
+
+            Console.WriteLine("Test Passed");
+            driver.Quit();
+        }
+    }
+}
